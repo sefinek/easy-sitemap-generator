3d8fb3a8d4e8bd60bc6f93b8b7bc6a7ad3c6e73b
Sefinek <contact@sefinek.net>
2024-11-02 18:33:48 +0100

    axios

--- services/axios.js
+++ services/axios.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const { version } = require('../package.json');
 
-axios.defaults.timeout = 24000;
 axios.defaults.headers.common['User-Agent'] = `Mozilla/5.0 (compatible; EasySitemapGen/${version}; +https://github.com/sefinek/easy-sitemap-generator)`;
+axios.defaults.timeout = 24000;
 
 module.exports = { axios, version };
