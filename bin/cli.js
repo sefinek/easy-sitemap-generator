4168eb5db142ecb99f7bf9f28c79e454f0884e6b
Sefinek <contact@sefinek.net>
2024-11-29 04:28:04 +0100

    Update cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -6,7 +6,7 @@ const { generate } = require('../lib/sitemapGenerator.js');
 const args = process.argv.slice(2);
 const urlArg = args.find(arg => arg.startsWith('--url='));
 if (!urlArg) {
-	logError('No URL provided. Use: sitemap-generator --url=<YOUR-DOMAIN>');
+	logError('No URL provided. Use: sitemap --url=<YOUR-DOMAIN>');
 	process.exit(1);
 }
 
