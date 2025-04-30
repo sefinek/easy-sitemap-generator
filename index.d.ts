665cb28614d797e5cf1ba376567a097029b71d79
Sefinek <contact@sefinek.net>
2025-04-30 12:38:56 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,10 @@
-declare module 'easy-sitemap-generator' {
-    /**
-     * Generates a sitemap for the specified webpage and saves it to the given destination.
-     *
-     * @param url - The URL of the webpage for which the sitemap is to be generated.
-     * @param destination - Optional. The path to the file where the generated sitemap will be saved.
-     * If not specified, the sitemap will be saved in the same folder where the script is run (default: './sitemap.xml').
-     * @returns A promise that resolves to a string containing the contents of the generated sitemap.xml file.
-     */
-    export function generate(url: string, destination?: string): Promise<string>;
+/**
+ * Generates a sitemap for the given URL and saves it to a file.
+ *
+ * @param url - The base URL to generate the sitemap for.
+ * @param destination - Optional path to save the sitemap file. Defaults to: `./sitemap.xml`
+ * @returns A promise that resolves with the contents of the generated sitemap.
+ */
+export function generate(url: string, destination?: string): Promise<string>;
 
-    /**
-     * The current version of the `easy-sitemap-generator` module.
-     */
-    export const version: string;
-}
+export const version: string;
